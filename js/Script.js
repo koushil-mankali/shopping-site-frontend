1ba938b0dd528e64763b14db38492ac2663b512c
Abhijith JR <Achuabhijithjr@outlook.com>
2024-05-06 15:56:44 +0530

    modify side meanu

--- js/Script.js
+++ js/Script.js
@@ -3,10 +3,7 @@ const menuToggle = document.getElementById('menu-toggle');
 const nav = document.querySelector('nav');
 
 menuToggle.addEventListener('click', () => {
-  // Toggle menu-toggle button class
   menuToggle.classList.toggle('open');
-
-  // Toggle nav visibility class
   nav.classList.toggle('show');
 });
 
